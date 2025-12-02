a6d9cea19c42abbceb0880d85b1dc9502651d3d6
Koray <128939343+dharmanan@users.noreply.github.com>
2025-12-02 18:48:01 +0000

    fix: Format BuidlGuidlLogo for prettier

--- packages/nextjs/components/assets/BuidlGuidlLogo.tsx
+++ packages/nextjs/components/assets/BuidlGuidlLogo.tsx
@@ -1,13 +1,5 @@
 import Image from "next/image";
 
 export const BuidlGuidlLogo = ({ className }: { className: string }) => {
-  return (
-    <Image
-      src="/logo.svg"
-      alt="Batch 22 Logo"
-      width={100}
-      height={100}
-      className={className}
-    />
-  );
+  return <Image src="/logo.svg" alt="Batch 22 Logo" width={100} height={100} className={className} />;
 };
